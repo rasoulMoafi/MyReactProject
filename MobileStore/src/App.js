77b34a22b9dc50c210583700475cd269241f6c20
rasoul <rasoulhk8@gmail.com>
2023-02-21 17:00:26 +0330

    modify build

--- MobileStore/src/App.js
+++ MobileStore/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Home from "./components/Home";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </BrowserRouter>
-
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
